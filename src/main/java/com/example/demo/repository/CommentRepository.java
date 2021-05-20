64fe7105a348862541dad6755b32f2cda2d7bc3e
Error4ik <fearerror@yandex.ru>
2021-05-21 00:58:57 +0300

    Repositories created.

---
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Comment;
+import com.example.demo.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findAllByPost(Post post);
+
+    Comment findByIdAndUserId(UUID id, UUID userId);
+}
---
+++ src/main/java/com/example/demo/repository/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, UUID> {
+
+    Optional<Image> findByUserId(UUID userId);
+
+    Optional<Image> findByPostId(UUID postId);
+}
