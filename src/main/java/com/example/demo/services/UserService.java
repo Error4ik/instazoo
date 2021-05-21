85cdb73f1a369d14d5f1a2f60fd3dc0a347093db
Error4ik <fearerror@yandex.ru>
2021-05-21 23:07:32 +0300

    Addede the ability to save new user.

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,46 @@
+package com.example.demo.services;
+
+import com.example.demo.entity.User;
+import com.example.demo.entity.enums.ERole;
+import com.example.demo.exceptions.UserExistException;
+import com.example.demo.payload.request.SignUpRequest;
+import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignUpRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setUsername(userIn.getUsername());
+        user.setSurname(userIn.getSurname());
+        user.setName(userIn.getName());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoleSet().add(ERole.ROLE_USER);
+
+        try {
+            LOGGER.info("Saving user {}", userIn.getEmail());
+            return this.userRepository.save(user);
+        } catch (Exception ex) {
+            LOGGER.error("Error during registration {}", ex.getMessage());
+            throw new UserExistException("The user already exist. Please check credentials.");
+        }
+    }
+}
